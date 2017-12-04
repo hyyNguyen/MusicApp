8b808f5a233e6951d008b07d9928ab5644d48203
hyyNguyen <hhuyqn@gmail.com>
2017-12-04 17:43:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asus/musicapp/Song.java
@@ -0,0 +1,39 @@
+package com.example.asus.musicapp;
+
+/**
+ * Created by ASUS on 12/04/17.
+ */
+
+
+public class Song {
+    private String name, path, album, artist;
+    private int duration;
+
+    public Song(String name, String path, String album, String artist, int duration) {
+        this.name = name;
+        this.path = path;
+        this.album = album;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
